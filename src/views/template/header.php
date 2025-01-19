42d7c12c771d768fb212fda837c5407e33d37b15
Cristian Morbidelli <cristian.morbidelli@studio.unibo.it>
2025-01-19 22:37:09 +0100

    aggiornamento navbar!!!!!!!!!!!!!!

--- src/views/template/header.php
+++ src/views/template/header.php
@@ -8,14 +8,12 @@ if(isset($_SESSION['email'])){
 }
 
 ?>
-<header class="d-flex align-items-center justify-content-between px-3 py-2">
-        <!-- Menu Button (pulsante menu a tendina) -->
-        <button class="btn btn-outline-primary menu-button" data-bs-toggle="collapse" data-bs-target="#menu">
-            ☰
+<nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top">
+  <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
         </button>
-
-        <!-- Logo accanto al titolo -->
-        <a href="?#" class="d-block py-2 text-decoration-none logo-header">
+        <a class="navbar-brand" href="?#">
             <div class="d-flex align-items-center">
                 <!-- Logo -->
                 <img src="<?php echo IMAGE_PATH; ?>logo/logo.png" alt="Logo" class="logo">
@@ -24,19 +22,34 @@ if(isset($_SESSION['email'])){
                 <h1 class="m-0 text-center flex-grow-1" style="font-size: 1.25rem; color: #6f42c1">MIAOH</h1>
             </div>
         </a>
+        <a class="navbar-brand" href="?action=profile">
+            <img src="<?php echo $picture ?>" alt="User" class="user-img"/>
+        </a>
 
-        <!-- User Image -->
-        <a href="?action=profile" class="d-block py-2 text-decoration-none"><img src="<?php echo $picture ?>" alt="User" class="user-img"/></a>
-    </header>
-
-<!-- Menu Dropdown -->
-<div id="menu" class="collapse bg-light">
-    <ul class="list-unstyled m-0 p-3">
-        <li><a href="?#" class="d-block py-2 text-decoration-none">Home</a></li>
-        <li><a href="#" class="d-block py-2 text-decoration-none">Shop</a></li>
-        <li><a href="?action=shopping&subAction=carrello#" class="d-block py-2 text-decoration-none">Cart</a></li>
-        <li><a href="?action=profile" class="d-block py-2 text-decoration-none">Profile</a></li>
-    </ul>
-</div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="?#">Home</a>
+            </li>
+            <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle active" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Shop
+            </a>
+            <ul class="dropdown-menu active">
+                <li><a class="dropdown-item" href="#">Accessori</a></li>
+                <li><a class="dropdown-item" href="#">Cibo</a></li>
+                <li><a class="dropdown-item" href="#">Giochi</a></li>
+            </ul>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" href="?action=shopping&subAction=carrello"><i class="bi bi-cart"></i> Carrello</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" href="?action=profile">Profilo</a>
+            </li>
+        </ul>
+        </div>
+    </div>
+</nav>
 
 <script src="public/script/header.js"></script>
